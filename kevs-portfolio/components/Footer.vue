23c611b08dd2622483f9f127ed7ee8d84f608a92
Kevin Hartono <kevinhartono2810@gmail.com>
2024-11-27 23:53:53 +0700

    refactor(footer): wording

--- kevs-portfolio/components/Footer.vue
+++ kevs-portfolio/components/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="bg-white">
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="hidden mx-auto lg:flex lg:flex-1">
-        <p class="text-sm/6 text-gray-900">© 2024 Kevin H., Inc. All rights reserved.</p>
+        <p class="text-sm/6 text-gray-900">© 2024 Kevin H., All rights reserved.</p>
       </div>
       <div class="hidden mx-auto lg:flex lg:gap-x-3">
         <a href="/">
